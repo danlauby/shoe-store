8b37fc08104341ef83760d8f3b601efbcbbf5736
danlauby <dmlauby@gmail.com>
2017-03-03 11:55:43 -0800

    Create root url route

---
+++ app/app.php
@@ -0,0 +1,31 @@
+<?php
+
+    date_default_timezone_set("America/Los_Angeles");
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Store.php";
+    require_once __DIR__."/../src/Brand.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost:3306;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app['debug'] = true;
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(),
+    array('twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    // Link to store or brand pages
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('index.html.twig', ['stores' => Store::getAll(), 'brands' => Brand::getAll()]);
+    });
+
+    return $app;
