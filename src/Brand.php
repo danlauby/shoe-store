beb9e394e3cfa030d6d6fd20fce03cf8c62785d0
danlauby <dmlauby@gmail.com>
2017-03-03 11:10:29 -0800

    Test addStore and getStores methods for brand class, pass

--- src/Brand.php
+++ src/Brand.php
@@ -63,4 +63,28 @@
             return $found_brand;
         }
 
+        function addStore($store)
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stores_brands (store_id, brand_id) VALUES ({$store->getId()}, {$this->getId()});");
+        }
+
+        function getStores()
+        {
+            $returned_stores = $GLOBALS['DB']->query("SELECT stores.* FROM brands
+                JOIN stores_brands ON (stores_brands.brand_id = brands.id)
+                JOIN stores ON (stores.id = stores_brands.store_id)
+                WHERE brands.id = {$this->getId()};");
+
+            $stores = [];
+            foreach($returned_stores as $store) {
+                $name = $store['name'];
+                $id = $store['id'];
+                $new_Store = new Store($name, $id);
+                array_push($stores, $new_Store);
+            }
+            return $stores;
+        }
+
+
+
     }
--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -6,7 +6,7 @@
     */
 
     require_once "src/Brand.php";
-    // require_once "src/Store.php";
+    require_once "src/Store.php";
 
     $server = 'mysql:host=localhost:3306;dbname=shoes_test';
     $username = 'root';
@@ -19,6 +19,7 @@
         protected function teardown()
         {
             Brand::deleteAll();
+            Store::deleteAll();
         }
 
         function testGetName()
@@ -136,6 +137,52 @@
 
             //Assert
             $this->assertEquals($test_Brand, $result);
+        }
+
+        function testAddStore()
+        {
+            //Arrange
+            $name = "Trents Shoes";
+            $id = 1;
+            $test_Store = new Store($name, $id);
+            $test_Store->save();
+
+
+            $brand_name = "Puma";
+            $id2 = 2;
+            $test_Brand = new Brand($brand_name, $id2);
+            $test_Brand->save();
+
+            // Act
+            $test_Brand->addStore($test_Store);
+            // Assert
+            $this->assertEquals($test_Brand->getStores(), [$test_Store]);
+        }
+
+        function testGetStores()
+        {
+            //Arrange
+            $name = "Puma";
+            $id = 1;
+            $test_Brand = new Brand($name, $id);
+            $test_Brand->save();
+
+            $store_name = "Trents Shoes";
+            $id2 = 2;
+            $new_Store = new Store($store_name, $id2);
+            $new_Store->save();
+
+            $store_name2 = "Adidas";
+            $id3 = 3;
+            $new_Store2 = new Store($store_name2, $id3);
+            $new_Store2->save();
+
+            // Act
+            $test_Brand->addStore($new_Store);
+            $test_Brand->addStore($new_Store2);
+
+            // Assert
+            $this->assertEquals($test_Brand->getStores(), [$new_Store, $new_Store2]);
         }
 
 
